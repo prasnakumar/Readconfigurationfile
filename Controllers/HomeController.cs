e50fa49db9b85954ba93595acf7e214b5733cb7c
Prasanna Kumar N <Prasanna.Augmento10@gmail.com>
2022-08-22 00:51:26 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Read_configuration_file.Helper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using Read_configuration_file.Services;
+
+namespace Read_configuration_file.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IOptions<UserConditions> _options;
+        public HomeController(ILogger<HomeController> logger , IOptions<UserConditions> options )
+        {
+            _logger = logger;
+            _options = options;
+        }
+        [HttpGet]
+        [Route("/Home")]
+        public IActionResult levelController(string input1, string error)
+        {
+           
+            string message ;
+            bool condition = false;
+        
+          
+            if (error.ToUpper() == "ERROR" && _options.Value.error)
+            {
+
+               
+                    _logger.LogError($"Error has occured {input1}");
+                    message= "Error Printed";
+                condition = true;
+
+
+
+
+            }
+
+            else if (error.ToUpper() == "WARNING" && _options.Value.warning)
+            {
+               
+                    _logger.LogWarning($"warning has occured  {input1}");
+                    message = "Warning Printed";
+                condition = true;
+               
+                
+
+            }
+
+            else if (error.ToUpper() == "DEBUG" && _options.Value.debug)
+            {
+                
+                    _logger.LogWarning($"warning has occured  {input1}");
+                    message = "Debug Printed";
+                condition = true;
+                
+            }
+
+            else
+            {
+                _logger.LogInformation($"NO issues {input1}");
+                message = "NOT Printed ";
+            }
+
+            return Ok(new { message= input1, HasConsolePrinted=message,LogType= error,LogCondidtionEnabled=condition });
+        }
+    }
+}
---
+++ Services/UserConditions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Read_configuration_file.Services
+{
+    public class UserConditions
+    {
+        public const string valueSection = "UserCondition";
+      
+        public bool error { get; set; }
+        public bool warning { get; set; }
+        public bool debug { get; set; }
+    }
+
+   
+}
